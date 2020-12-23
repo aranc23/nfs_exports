dce3bb2ad637386dbd4fa90646ad0beae0cbf3a8
Aran Cox <arancox@gmail.com>
2020-12-23 10:21:17 -0600

    functionally complete, with puppet strings

--- manifests/export.pp
+++ manifests/export.pp
@@ -3,32 +3,38 @@
 # Manages one export entry in /etc/exports,
 # completelely replacing any existing hosts for that entry.
 #
-# @example
-#   nfs_exports::export { '/data':
-#    clients => [{client => 'example.com'},
-#                {client => 'other.example.com',
-#                 options => 'rw'} ],
-#     default_options => ['ro'],
-#    }
+# @param clients
+#   struct containing a client string (netgroup, ip address, etc.)
+#   and an optional options array.  If the client is - it is treated
+#   as a defaults entry and the options are not enclosed in parenthesis
+#   see the exports man page on your system for details
+# 
+# @example Typical usage
+# nfs_exports::export { '/data':
+#   clients => [
+#     { client => '-', options => ['ro'] },
+#     { client => 'example.com'},
+#     { client => 'other.example.com', options => 'rw'},
+#  ],
+# }
+# should produce:
+#   /data -ro example.com other.example.com(rw)
 define nfs_exports::export
 (
-  String $etc_exports = '/etc/exports',
-  String $exportfs = 'exportfs -ra',
-  Array[String] $exportfs_path = ['/sbin','/usr/sbin'],
   Array[Struct[{
     client  => String,
     options => Optional[Variant[String,Array[String]]]
   }]] $clients = [],
-  Optional[Variant[String,Array[String]]] $default_options = [],
 )
 {
-  file_line { "update export entry for ${title} in ${etc_exports}":
-    path  => $etc_exports,
-    line  => template('nfs_exports/export.erb'),
-    match => "^${title}\s*",
+  include ::nfs_exports
+  assert_type(Stdlib::Absolutepath,$title)
+
+  file_line { "update export entry for ${title} in ${nfs_exports::etc_exports}":
+    path   => $nfs_exports::etc_exports,
+    line   => template('nfs_exports/export.erb'),
+    match  => "^${title}\s*",
+    notify => Exec[$nfs_exports::exportfs]
   }
-  ~> exec { $exportfs:
-    refreshonly => true,
-    path        => $exportfs_path,
-  }
+
 }
---
+++ manifests/init.pp
@@ -0,0 +1,46 @@
+# @summary manage entries in /etc/exports
+#
+# Along with a defined type (nfs::exports::export) manages 
+# entries in /etc/exports and subsequently invokes exportfs.
+#
+# @param etc_exports
+#   path to file to apply changes to (/etc/exports)
+# @param exportfs
+#   command to invoke after changes to exports file
+# @param exportfs_path
+#   passed to the exec used to invoke exportfs
+# @param exports
+#   hash with index of the path of the export, and a key
+#   consisting of an array of client structs, see below for example
+#
+# @example Class usage
+# class { '::nfs_exports':
+#   exports => {
+#     '/data'     => [
+#       { client => 'testvm.example.com', options => ['rw'] },
+#       { client => 'roclient.example.com', options => ['ro'] },
+#     ],
+#   },
+# }
+class nfs_exports
+(
+  String $etc_exports,
+  String $exportfs,
+  Array[String] $exportfs_path,
+  Hash[String,Array[Struct[{
+    client  => String,
+    options => Optional[Variant[String,Array[String]]]
+  }]]] $exports = {}
+)
+{
+  $exports.each |String $path,Array $clients| {
+    nfs_exports::export { $path:
+      clients => $clients,
+    }
+  }
+  exec { $exportfs:
+    refreshonly => true,
+    path        => $exportfs_path,
+  }
+}
+
