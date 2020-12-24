abc3a5d6ed28edf0d05f3e58d539337bab5eef5d
Aran Cox <arancox@gmail.com>
2020-12-24 12:57:17 -0600

    add more tests with parameters

--- spec/defines/export_spec.rb
+++ spec/defines/export_spec.rb
@@ -15,4 +15,24 @@ describe 'nfs_exports::export' do
       it { is_expected.to compile }
     end
   end
+  context 'export /data to testvm' do
+    let(:title) { '/data' }
+    let(:params) { { 'clients' => [{ 'client' => 'testvm' }]}}
+    it do
+      is_expected.to contain_file_line('update export entry for /data in /etc/exports').with(
+        {'line' => '/data testvm', 'path' => '/etc/exports' })
+    end
+  end
+  context 'export /data to testvm testvm2 with defaults and options' do
+    let(:title) { '/data' }
+    let(:params) { { 'clients' => [
+      { 'client' => '-', 'options' => 'ro' },
+      { 'client' => 'testvm'},
+      { 'client' => 'testvm2', 'options' => 'rw'},
+      ]}}
+    it do
+      is_expected.to contain_file_line('update export entry for /data in /etc/exports').with(
+        {'line' => '/data -ro testvm testvm2(rw)', 'path' => '/etc/exports' })
+    end
+  end
 end
