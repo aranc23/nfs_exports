44cb215ef9dd26d1a769031cefa845e4ca2fa598
Aran Cox <arancox@gmail.com>
2020-12-24 12:30:34 -0600

    must contain an exec

--- spec/classes/nfs_exports_spec.rb
+++ spec/classes/nfs_exports_spec.rb
@@ -6,8 +6,8 @@ describe 'nfs_exports' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
       it { is_expected.to compile }
+      it { is_expected.to contain_exec('exportfs -ra') }
     end
   end
 end
